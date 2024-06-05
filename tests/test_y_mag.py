5e4f22d2a36f586146f240d2fb5dd822dc2faa81
Graham Bevan <graham.bevan@ntlworld.com>
2024-06-05 18:47:19 +0100

    y mag db

--- tests/test_y_mag.py
+++ tests/test_y_mag.py
@@ -1,5 +1,5 @@
 from ycx_complex_numbers import Y, Z
-from ycx_rf_amplifiers.y_params import y_max_available_gain
+from ycx_rf_amplifiers.y_params import y_max_available_gain, y_max_available_gain_db
 
 
 def test_good_inputs():
@@ -21,3 +21,11 @@ def test_bad_inputs():
         print(e)
     else:
         assert False
+
+def test_mag_db():
+    yi = Y(14 + 1j)
+    yo = Y(0.2 + 2j)
+    yf = Y(-14 + 0.8j)
+
+    mag_db = round(y_max_available_gain_db(yi, yo, yf), 2)
+    assert mag_db == 12.44
--- ycx_rf_amplifiers/y_params/max_available_gain.py
+++ ycx_rf_amplifiers/y_params/max_available_gain.py
@@ -1,6 +1,18 @@
+import math
 from ycx_complex_numbers import Complex, Y
 
 def y_max_available_gain(yi=None, yo=None, yf=None):
+    """
+     Calculate the Maximum Available Gain for an RF Amplifier
+
+    Args:
+        yi (Complex): Input Admittance
+        yo (Complex): Output Admittance
+        yf (Complex): Forward-Transfer Admittance
+
+    Returns:
+        float: The Maximum Available Gain.
+    """
     for p in (yi, yo, yf):
         if not isinstance(p, Y):
             raise TypeError("All admittance inputs must be type Y Complex number instances")
@@ -11,3 +23,17 @@ def y_max_available_gain(yi=None, yo=None, yf=None):
     mag = (abs(yf) ** 2) / (4 * gi * go)
 
     return mag
+
+def y_max_available_gain_db(yi=None, yo=None, yf=None):
+    """
+     Calculate the Maximum Available Gain in Decibels for an RF Amplifier
+
+    Args:
+        yi (Complex): Input Admittance
+        yo (Complex): Output Admittance
+        yf (Complex): Forward-Transfer Admittance
+
+    Returns:
+        float: The Maximum Available Gain in Decibels.
+    """
+    return 10 * math.log10(y_max_available_gain(yi=yi, yo=yo, yf=yf))
